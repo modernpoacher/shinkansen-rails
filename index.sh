34d3f302d2f74cbe21ae7dd19382f4b5f4b8977a
Jonathan Perry <modernpoacher@modernpoacher.com>
2018-06-17 23:01:43 +0100

    `package.json` `index.sh`

--- index.sh
+++ index.sh
@@ -2,15 +2,15 @@
 
 PASS="\x1B[0;32m\x20\x20✔\x1B[0m" # GREEN
 FAIL="\x1B[0;31m\x20\x20✕\x1B[0m" # RED
-MODULE="\x1B[0;90m(shinkansen-rails)\x1B[0m" # GREY
+MODULE="\x1B[0;90mshinkansen-rails\x1B[0m" # GREY
 
 if [[ $? -eq 0 ]]; then
 
-  cd ../shinkansen-rails
-  rm -rf lib
-  npm install
+  # cd ../shinkansen-rails
+  npm run clean &> /dev/null
+  npm install &> /dev/null
 
-  echo -e "\x15" # CR
+  # echo -e "\x15" # CR
   echo -e $PASS $MODULE
 
   echo -e "\x15" # CR
