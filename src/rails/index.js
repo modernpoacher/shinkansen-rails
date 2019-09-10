5d7a5986087d05edcc907f6ff4e61bbf91ab5a52
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-09-10 12:39:46 +0100

    Mocha

--- src/rails/index.js
+++ src/rails/index.js
@@ -1,5 +1,3 @@
-/* eslint no-useless-escape: 0 */
-
 let pattern
 
 const map = new Map()
@@ -9,17 +7,25 @@ const PATTERN = '/:alpha/:omega'
 const CHAR32 = String.fromCharCode(32)
 const CHAR45 = String.fromCharCode(45)
 
-const any = (o) => !!Reflect.ownKeys(o).length
-const has = (o, k) => Reflect.has(o, k)
-const get = (o, k) => Reflect.get(o, k)
+export const any = (o) => !!Reflect.ownKeys(o).length
+export const has = (o, k) => Reflect.has(o, k)
+export const get = (o, k) => Reflect.get(o, k)
 
-// const toString = (s) => (s || `${s}`).toString()
+/**
+ *  Format simple latin character strings as URL compatible
+ *
+ *  @param {String} s   The string to convert
+ *  @return {String}    The string, converted
+ */
+export const rail = (s) => (
+  s.toLowerCase().replace(/[^\w\-\d]/g, CHAR32).trim().replace(/[\s]+/g, CHAR45).replace(/[\s\s|\-\-]+/g, CHAR45)
+)
 
 /**
  *  Interrogate parameters to determine whether or not components can be created
  *
- *  @param {String} s   The 'pattern' according to which URLs should be created
  *  @param {Object} o   The object containing values for the 'pattern'
+ *  @param {String} s   The 'pattern' according to which URLs should be created
  *  @param {Boolean} b  Whether the Rails can go
  *  @return {Boolean}
  *
@@ -32,64 +38,38 @@ const get = (o, k) => Reflect.get(o, k)
  *          set the boolean = whether the object has a property for that key
  *            return that boolean (to the loop. It doesn't exit here)
  *      Else set the boolean as false and return it (to the loop. It doesn't exit here, either)
- *  Return the boolean (on the other side of the '||'' since 'forEach' returns undefined)
+ *  Return the boolean
  *
- *  Was:
- *
- *    const go = (s, o, b) => (
- *      s.match(/(\w+)/g).forEach((k) => (k in o) ? b ? (b = !!o[k]) : b : (b = !!0)) || b
- *    )
- *
- *  Otherwise:
- *
- *    const go = (s, o, b) => {
- *      s.match(/(\w+)/g)
- *        .forEach((k) => {
- *          if (Reflect.has(o, k)) {
- *            if (b) {
- *              b = !!Reflect.get(o, k)
- *            }
- *          } else {
- *            b = false
- *          }
- *        })
- *      return b
- *    }
- *
- *  Now (equivalent to):
- *
- *    const go = (s, o, b) => {
- *      const a = s.match(/(\w+)/g)
- *      let k
- *      while (b && (k = a.shift())) {
- *        b = (Reflect.has(o, k)) ? !!Reflect.get(o, k) : false
- *      }
- *      return b
- *    }
- *
- *  (Since 'forEach' does not break, while a 'while' will)
+ *  A `reduce` cannot break when `b` is false
  */
-const go = (o, s) => { // babili fails
+export const go = (o, s) => {
   let b = any(o)
   if (b) {
     let k
     const a = s.match(/(\w+)/g)
-    while (b && (k = a.shift())) {
-      b = has(o, k) ? !!get(o, k) : false
-    }
+    while (b && (k = a.shift())) b = has(o, k) // it is only necessary that the `o` has key `k`. Otherwise, `b = has(o, k) && !!get(o, k)` to enforce a truthy field value for field key `k`
   }
-  return b // babili fails
+  return b
 }
 
 /**
- *  Format simple latin character strings as URL compatible
+ *  Interrogate parameters for object field names
  *
- *  @param {String} s   The string to convert
- *  @return {String}    The string, converted
+ *  @param {Object} o   The object containing fields
+ *  @param {String} s   The string containing field names to be found
+ *  @return {String}
+ *
+ *  @description
+ *
+ *  A valid object is
+ *
+ *    { fieldName: 'field Value' }
+ *
+ *  A valid string is
+ *
+ *    /:fieldName
  */
-const rail = (s) => (
-  s.toLowerCase().replace(/[^\w\-\d]/g, CHAR32).trim().replace(/[\s]+/g, CHAR45).replace(/[\s\s|\-\-]+/g, CHAR45)
-)
+export const to = (o, s) => s.replace(/(?::)(\w+)/g, (m, k) => m ? has(o, k) ? get(o, k) : m : m)
 
 /**
  *  Rails.go()  Boolean
@@ -108,15 +88,11 @@ export default class Rails {
     return S
   }
 
-  static go = (o = {}, s = Rails.pattern()) => ( // eslint-disable-next-line no-cond-assign
+  static go = (o = {}, s = Rails.pattern()) => (
     any(o) ? (s = String(s)) ? go(o, s) : false : false // return is s is truthy true then go else false
   )
 
   static to = (o = {}, s = Rails.pattern()) => (
-    any(o)
-      ? String(s).replace(/(?::)(\w+)/g, (m, k) => ( // can't pull this out into a const because 'o' must be in scope
-        m ? has(o, k) ? get(o, k) : m : m
-      ))
-      : String(s)
+    any(o) ? (s = String(s)) ? to(o, s) : s : s
   )
 }
